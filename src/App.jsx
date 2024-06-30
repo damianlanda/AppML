0cd11324c059e3946c8a273d9e442cb5d494f973
milton guzman <116039838+miltonx24@users.noreply.github.com>
2024-06-30 18:54:50 -0300

    detalle v1.4

--- src/App.jsx
+++ src/App.jsx
@@ -53,10 +53,7 @@ const App = () => {
     <Router>
       <div>
         <nav>
-          <Link to="/" onClick={handleHomeClick}>
-            <img src="/images/logo.webp" alt="Logo" />
-          </Link>
-          <SearchBar onSearch={handleSearch} />
+          <Link to="/" onClick={handleHomeClick}>Inicio</Link>
           <Link to="/cart">Carrito</Link>
         </nav>
 
@@ -65,6 +62,7 @@ const App = () => {
             path="/"
             element={
               <>
+                <SearchBar onSearch={handleSearch} />
                 {selectedProduct ? (
                   <ProductDetails
                     productId={selectedProduct}
